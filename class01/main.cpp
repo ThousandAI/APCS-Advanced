169a245e9f97eac8568e9b6373c0ae317284270d
ThousandAI <ceo@thousandai.com>
2023-02-14 00:01:17 +0800

    basic c++ syntax

---
+++ class01/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 資料型態
+    /*
+    char grade = 'A';
+    int n1 = 12;
+    float n2 = 3.2;
+    double n3 = 6.8;
+    bool isGood = true;
+    string host = "Thousand";
+    */
+
+    // 輸入/輸出
+    /*
+    int age;
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Your name is: " << name << ", and age is: " << age << endl;
+    */
+
+    // 字串
+    /*
+    string slogan = "Welcome To APCS-Advanced";
+    cout << "String length: " << slogan.length() << endl;
+    for(int i = 0; i < slogan.length(); ++i){
+        cout << slogan[i];
+    }
+    cout << endl;
+    */
+
+    // string cin getline
+    /*
+    string name;
+    //cout << "Enter your name: ";
+    //cin >> name;
+    //cout << "Your name is: " << name << endl;
+
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Your name is: " << name << endl;
+    */
+
+    return 0;
+}
