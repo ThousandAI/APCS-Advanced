597f5f4843dfb2ad453c7fcaaa7ecd9f19a08658
ThousandAI <ceo@thousandai.com>
2023-02-14 17:20:40 +0800

    Reference

--- class01/main.cpp
+++ class01/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-
 using namespace std;
 
+// 資料型態
+/*
 int main()
 {
-    // 資料型態
-    /*
     char grade = 'A';
     int n1 = 12;
     float n2 = 3.2;
     double n3 = 6.8;
     bool isGood = true;
     string host = "Thousand";
-    */
+    return 0;
+}
+*/
 
-    // 輸入/輸出
-    /*
+// 輸入/輸出
+/*
+int main(){
     int age;
     string name;
     cout << "Enter your name: ";
@@ -23,29 +25,47 @@ int main()
     cout << "Enter your age: ";
     cin >> age;
     cout << "Your name is: " << name << ", and age is: " << age << endl;
-    */
+    return 0;
+}
+*/
 
-    // 字串
-    /*
+// 字串
+/*
+int main(){
     string slogan = "Welcome To APCS-Advanced";
     cout << "String length: " << slogan.length() << endl;
     for(int i = 0; i < slogan.length(); ++i){
         cout << slogan[i];
     }
     cout << endl;
-    */
 
-    // string cin getline
-    /*
     string name;
-    //cout << "Enter your name: ";
-    //cin >> name;
-    //cout << "Your name is: " << name << endl;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Your name is: " << name << endl;
 
     cout << "Enter your name: ";
     getline(cin, name);
     cout << "Your name is: " << name << endl;
-    */
-
     return 0;
 }
+*/
+
+// Reference(參考)
+/*
+void swap(int &a, int &b){
+    int temp = a;
+    a = b ;
+    b = temp;
+}
+
+int main(){
+    int a = 3, b= 5;
+    swap(a, b);
+    cout << "a is: " << a << ", b is: " << b << endl;
+    return 0;
+}
+*/
+
+
+
